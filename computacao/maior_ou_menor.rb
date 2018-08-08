691a269edd602552d60a3e7ae9f28d1cd67a8c8b
marcuspereiragithub <vinnycoast@gmail.com>
2018-08-08 16:24:28 -0300

    Adding condition and score

--- computacao/maior_ou_menor.rb
+++ computacao/maior_ou_menor.rb
@@ -1,27 +1,28 @@
 def da_boas_vindas
     puts "Bem vindo ao jogo da adivinhação"
     puts "Qual é o seu nome:"
-    nome = gets
+    nome = gets.strip
 
     puts "\n\n"
 
-    puts "Começaremos o jogo para você, " + nome
+    puts "Começaremos o jogo para você, #{nome}"
 end
 
 def sorteia_numero_secreto
     puts "Escolhendo um número secreto entre 0 e 200..."
-    sorteado = 175
+    sorteado = rand(200)
     puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
     sorteado #Em ruby não é necessário colocar o return
              #Por padrão ele retorna a última linha
 end
 
-def pede_um_numero(tentativas, limite_de_tentativas)
+def pede_um_numero(chutes, tentativas, limite_de_tentativas)
     puts "\n\n"
-    puts "Tentativa " + tentativas.to_s + " de " + limite_de_tentativas.to_s
+    puts "Tentativa #{tentativas} de #{limite_de_tentativas}"
+    puts "Chutes até agora: #{chutes}"
     puts "Entre com o número"
-    chute = gets
-    puts "Será que você acertou? Você chutou " + chute
+    chute = gets.strip
+    puts "Será que você acertou? Você chutou #{chute}"
     chute.to_i
 end
 
@@ -45,11 +46,18 @@ end
 
 da_boas_vindas
 numero_secreto = sorteia_numero_secreto
-
+pontos_ate_agora = 1000
 limite_de_tentativas = 5
+chutes = []
+
 for tentativas in 1..limite_de_tentativas
-    chute = pede_um_numero(tentativas, limite_de_tentativas)
+    chute = pede_um_numero(chutes, tentativas, limite_de_tentativas)
+    chutes << chute
+    pontos_a_perder = (chute - numero_secreto).abs / 2.0
+    pontos_ate_agora -= pontos_a_perder
     if verifica_se_acertou(numero_secreto, chute)
         break  
     end
 end
+
+puts "Você ganhou #{pontos_ate_agora} pontos!"
