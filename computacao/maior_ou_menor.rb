62e91988165c866ad7bab40f181268453c3e7307
marcuspereiragithub <vinnycoast@gmail.com>
2018-08-09 15:31:04 -0300

    finalized

--- computacao/maior_ou_menor.rb
+++ computacao/maior_ou_menor.rb
@@ -1,4 +1,14 @@
 def da_boas_vindas
+    puts
+    puts "        P  /_\  P                              "
+    puts "       /_\_|_|_/_\                             "
+    puts "   n_n | ||. .|| | n_n         Bem vindo ao    "
+    puts "   |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!"
+    puts "  |' '  |  |_|  |'  ' |                        "
+    puts "  |_____| ' _ ' |_____|                        " 
+    puts "        \__|_|__/                              "
+    puts
+
     puts "Bem vindo ao jogo da adivinhação"
     puts "Qual é o seu nome:"
     nome = gets.strip
@@ -6,11 +16,33 @@ def da_boas_vindas
     puts "\n\n"
 
     puts "Começaremos o jogo para você, #{nome}"
+    nome
 end
 
-def sorteia_numero_secreto
-    puts "Escolhendo um número secreto entre 0 e 200..."
-    sorteado = rand(200)
+def pede_dificuldade
+    puts "Qual o nível de dificuldade?"
+    puts "(1) Muito fácil (2) Fácil (3) Normal (4) Difícil (5) Impossível"
+    puts "Escolha: "
+    dificuldade = gets.to_i
+end
+
+
+def sorteia_numero_secreto(dificuldade)
+    case dificuldade
+    when 1
+        maximo = 30
+    when 2
+        maximo = 60
+    when 3
+        maximo = 100
+    when 4
+        maximo = 150
+    else
+        maximo = 200
+    end
+
+    puts "Escolhendo um número secreto entre 1 e #{maximo}..."
+    sorteado = rand(maximo) - 1
     puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
     sorteado #Em ruby não é necessário colocar o return
              #Por padrão ele retorna a última linha
@@ -26,11 +58,33 @@ def pede_um_numero(chutes, tentativas, limite_de_tentativas)
     chute.to_i
 end
 
+def ganhou
+    puts
+    puts "             OOOOOOOOOOO               "
+    puts "         OOOOOOOOOOOOOOOOOOO           "
+    puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
+    puts "    OOOOOO      OOOOO      OOOOOO      "
+    puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
+    puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  "
+    puts " OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   "
+    puts "  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    "
+    puts "    OOOOO   OOOOOOOOOOOOOOO   OOOO     "
+    puts "      OOOOOO   OOOOOOOOO   OOOOOO      "
+    puts "         OOOOOO         OOOOOO         "
+    puts "             OOOOOOOOOOOO              "
+    puts
+    puts "               Acertou!                "
+    puts
+end
+
 def verifica_se_acertou(numero_secreto, chute)
     acertou = numero_secreto == chute
 
     if acertou
-        puts "Você acertou!"
+        ganhou
         return true
     end 
 
@@ -43,21 +97,42 @@ def verifica_se_acertou(numero_secreto, chute)
     false
 end
 
+def joga(nome, dificuldade)
+    numero_secreto = sorteia_numero_secreto dificuldade
+    pontos_ate_agora = 1000
+    limite_de_tentativas = 5
+    chutes = []
 
-da_boas_vindas
-numero_secreto = sorteia_numero_secreto
-pontos_ate_agora = 1000
-limite_de_tentativas = 5
-chutes = []
-
-for tentativas in 1..limite_de_tentativas
-    chute = pede_um_numero(chutes, tentativas, limite_de_tentativas)
-    chutes << chute
-    pontos_a_perder = (chute - numero_secreto).abs / 2.0
-    pontos_ate_agora -= pontos_a_perder
-    if verifica_se_acertou(numero_secreto, chute)
-        break  
+    for tentativas in 1..limite_de_tentativas
+        chute = pede_um_numero(chutes, tentativas, limite_de_tentativas)
+        chutes << chute
+        if nome == "marcus"
+            ganhou
+            break
+        end
+        pontos_a_perder = (chute - numero_secreto).abs / 2.0
+        pontos_ate_agora -= pontos_a_perder
+        if verifica_se_acertou(numero_secreto, chute)
+            break  
+        end
     end
+
+    puts "Você ganhou #{pontos_ate_agora} pontos!"
 end
 
-puts "Você ganhou #{pontos_ate_agora} pontos!"
+def nao_quer_jogar
+    puts "Deseja jogar novamente? (S/N)"
+    quero_jogar = gets.strip
+    nao_quero_jogar = quero_jogar.upcase == "N"
+end
+
+
+nome = da_boas_vindas
+dificuldade = pede_dificuldade
+
+loop do
+    joga(nome, dificuldade)
+    if nao_quer_jogar
+        break
+    end
+end
